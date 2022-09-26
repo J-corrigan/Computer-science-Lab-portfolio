6b640487bee5ecbbe289aa10e6732298660e2062
J-corrigan <114403411+J-corrigan@users.noreply.github.com>
2022-09-26 14:25:54 +0100

    portfolio 1
    first 5 tasks completed as part of portfolio

---
+++ lab1.py
@@ -0,0 +1,23 @@
+import math as m
+
+# TASK 1
+print("hello world")
+
+# TASK 2
+print("hello Joshua!")
+
+# TASK 3
+C = 38.4
+F = (C * 1.8) + 32
+print(
+    C, "celsius",
+    F, "fahrenheit"
+)
+
+# TASK 4
+print(48426 // (1014 - 162))
+
+# TASK 5
+print("Lab Group 1:", m.ceil(113 // 24), "groups", 113 % 24, "left over")
+print("Lab Group 2:", m.ceil(175 // 24), "groups", 175 % 24, "left over")
+print("Lab Group 3:", m.ceil(12 / 24), "groups", 0, "left over")
